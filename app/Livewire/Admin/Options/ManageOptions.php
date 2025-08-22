09c3ce1e04e30a97b0580b843a7522c3208584d0
Javier Borjas <cristman11@gmail.com>
2025-08-21 23:57:30 -0300

    Crear opciones

--- app/Livewire/Admin/Options/ManageOptions.php
+++ app/Livewire/Admin/Options/ManageOptions.php
@@ -10,9 +10,36 @@ class ManageOptions extends Component
 
     public $options;
 
+    public $newOption = [
+        'name' => '',
+        'type' => 1,
+        'features' => [
+            [
+                'value' => '',
+                'description' => ''
+            ]
+        ]
+    ];
+
+    public $openModal = true;
+
     public function mount()
     {
         $this->options = Option::with('features')->get();
+    }
+
+    public function addFeature()
+    {
+        $this->newOption['features'][] = [
+            'value' => '',
+            'description' => '',
+        ];
+    }
+
+    public function removeFeature($index)
+    {
+        unset($this->newOption['features'][$index]);
+        $this->newOption['features'] = array_values($this->newOption['features']);
     }
 
     public function render()
--- resources/views/livewire/admin/options/manage-options.blade.php
+++ resources/views/livewire/admin/options/manage-options.blade.php
@@ -3,9 +3,15 @@
     <section class="rounded-lg bg-white shadow-lg">
 
         <header class="border-b border-gray-200 px-6 py-2">
-            <h1 class="text-lg font-semibold text-gray-700">
-                Opciones
-            </h1>
+            <div class="flex justify-between">
+                <h1 class="text-lg font-semibold text-gray-700">
+                    Opciones
+                </h1>
+
+                <x-button wire:click="$set('openModal', true)">
+                    Nuevo
+                </x-button>
+            </div>
         </header>
 
         <div class="p-6">
@@ -13,13 +19,12 @@
             <div class="space-y-6">
 
                 @foreach ($options as $option)
-
                     <div class="p-6 rounded-lg border border-gray-200 relative">
 
                         <div class="absolute -top-3 px-4 bg-white">
 
                             <span>
-                                {{  $option->name }}
+                                {{ $option->name }}
                             </span>
 
                         </div>
@@ -28,28 +33,23 @@
                         <div class="flex flex-wrap">
 
                             @foreach ($option->features as $feature)
-
                                 @switch($option->type)
                                     @case(1)
-
-                                        <span class="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-gray-700 dark:text-gray-300">
-                                            {{ 
-                                                $feature->description
-                                             }}
+                                        <span
+                                            class="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-gray-700 dark:text-gray-300">
+                                            {{ $feature->description }}
                                         </span>
-                                        
-                                        @break
+                                    @break
+
                                     @case(2)
-
                                         {{-- Color --}}
-                                        <span class="inline-block h-6 w-6 shadow-lg rounded-full border-2 border-gray-300 mr-4" style="background-color: {{ $feature->value  }}">
+                                        <span class="inline-block h-6 w-6 shadow-lg rounded-full border-2 border-gray-300 mr-4"
+                                            style="background-color: {{ $feature->value }}">
                                         </span>
-                                        
-                                        @break
+                                    @break
+
                                     @default
-                                        
                                 @endswitch
-                                
                             @endforeach
 
                         </div>
@@ -62,4 +62,110 @@
         </div>
 
     </section>
+
+    <x-dialog-modal wire:model="openModal">
+
+        <x-slot name="title">
+            Crear nueva opción
+        </x-slot>
+
+        <x-slot name="content">
+
+            <div class="grid grid-cols-2 gap-6 mb-4">
+
+                <div>
+                    <x-label class="mb-1">
+                        Nombre
+                    </x-label>
+
+                    <x-input wire:model="newOption.name" class="w-full" placeholder="Por ejemplo: Tamaño, Color" />
+                </div>
+
+                <div>
+                    <x-label class="mb-1">
+                        Tipo
+                    </x-label>
+
+                    <x-select wire:model.live="newOption.type" class="w-full">
+                        <option value="1">Texto</option>
+                        <option value="2">Color</option>
+                    </x-select>
+                </div>
+
+            </div>
+
+            <div class="flex items-center mb-4">
+                <hr class="flex-1">
+
+                <span class="mx-4">
+                    Valores
+                </span>
+
+                <hr class="flex-1">
+            </div>
+
+            <div class="mb-4 space-y-4">
+                @foreach ($newOption['features'] as $index => $feature)
+                    <div class="p-6 rounded-lg border border-gray-200 relative" wire:key="features-{{ $index }}">
+
+                        <div class="absolute -top-3 px-4 bg-white">
+                            <button wire:click="removeFeature({{ $index }})">
+                                <i class="fa-solid fa-trash-can text-red-500 hover:text-red-600"></i>
+                            </button>
+                        </div>
+
+                        <div class="grid grid-cols-2 gap-6">
+
+                            <div>
+                                <x-label class="mb-1">
+                                    Valor
+                                </x-label>
+
+                                @switch($newOption['type'])
+                                    @case(1)
+                                        <x-input wire:model="newOption.features.{{ $index }}.value" class="w-full"
+                                            placeholder="Ingrese el valor de la opción" />
+                                    @break
+
+                                    @case(2)
+                                        <div class="border border-gray-300 rounded-md h-[42px] flex items-center px-3">
+                                            {{ 
+                                                $newOption['features'][$index]['value']
+                                            }}
+                                            <input type="color" wire:model="newOption.features.{{ $index }}.value">
+                                        </div>
+                                    @break
+
+                                    @default
+                                @endswitch
+                            </div>
+
+                            <div>
+                                <x-label class="mb-1">
+                                    Descripción
+                                </x-label>
+
+                                <x-input wire:model="newOption.features.{{ $index }}.description" class="w-full"
+                                    placeholder="Ingrese una descripción" />
+                            </div>
+
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+
+            <div class="flex justify-end">
+
+                <x-button wire:click="addFeature">
+                    Agregar valor
+                </x-button>
+
+            </div>
+
+        </x-slot>
+
+        <x-slot name="footer">
+        </x-slot>
+
+    </x-dialog-modal>
 </div>

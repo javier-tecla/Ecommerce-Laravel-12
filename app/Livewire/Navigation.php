782bf3a7951e5df3ffc43abbc27e66fd11030644
Javier Borjas <cristman11@gmail.com>
2025-09-08 23:09:57 -0300

    Diseño del menu 1

--- app/Livewire/Navigation.php
+++ app/Livewire/Navigation.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class Navigation extends Component
 {
+    public $families;
+
+    public function mount()
+    {
+        $this->families = \App\Models\Family::all();
+    }
+
     public function render()
     {
         return view('livewire.navigation');

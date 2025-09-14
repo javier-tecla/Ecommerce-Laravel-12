c21d55a3bd9b4b9e8f833a4e208e0d08d1c54555
Javier Borjas <cristman11@gmail.com>
2025-09-13 23:49:59 -0300

    Listar portadas

--- resources/views/admin/covers/index.blade.php
+++ resources/views/admin/covers/index.blade.php
@@ -16,4 +16,60 @@
 
     </x-slot>
 
+    <ul class="space-y-4">
+        @foreach ($covers as $cover)
+            <li class="bg-white rounded-lg shadow-lg overflow-hidden lg:flex">
+
+                <img src="{{ $cover->image }}" alt="" class="w-full lg:w-64 aspect-[3/1] object-cover object-center">
+
+                <div class="p-4 lg:flex-1 lg:flex lg:justify-between lg:items-center space-y-3 lg:space-y-0">
+
+                    <div>
+                        <h1 class="font-semibold">
+                            {{ $cover->title }}
+                        </h1>
+
+                        <p>
+                            @if ($cover->is_active)
+                                <span
+                                    class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-green-900 dark:text-green-300">Activo</span>
+                            @else
+                                <span
+                                    class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-red-900 dark:text-red-300">Inactivo</span>
+                            @endif
+                        </p>
+                    </div>
+
+                    <div>
+                        <p class="text-sm font-bold">
+                            Fecha de inicio
+                        </p>
+
+                        <p>
+                            {{ $cover->start_at->format('d/m/y') }}
+                        </p>
+                    </div>
+
+                    <div>
+                        <p class="text-sm font-bold">
+                            Fecha de finalización
+                        </p>
+
+                        <p>
+                            {{ $cover->end_at ? $cover->end_at->format('d/m/y') : '-' }}
+                        </p>
+                    </div>
+
+                    <div>
+                        <a class="btn btn-blue" href="{{ route('admin.covers.edit', $cover) }}">
+                            Editar
+                        </a>
+                    </div>
+
+                </div>
+            </li>
+        @endforeach
+
+    </ul>
+
 </x-admin-layout>

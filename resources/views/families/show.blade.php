c2390a573f39013b88826094b39f10e9ce5626a8
Javier Borjas <cristman11@gmail.com>
2025-09-15 22:50:02 -0300

    Mostrar opciones y features por familia II

--- resources/views/families/show.blade.php
+++ resources/views/families/show.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
 
-    @livewire('filter', [
-        'family_id' =>  $family->id,
-    ])
+    <div class="mt-12">
+        @livewire('filter', [
+            'family_id' => $family->id,
+        ])
+    </div>
 
 </x-app-layout>

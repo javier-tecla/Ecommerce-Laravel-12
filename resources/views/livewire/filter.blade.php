5bbb738f1b4dadb8491d3b6b53bba10f0a1de6c3
Javier Borjas <cristman11@gmail.com>
2025-09-15 22:59:39 -0300

    Mostrar y ocultar features

--- resources/views/livewire/filter.blade.php
+++ resources/views/livewire/filter.blade.php
@@ -6,16 +6,22 @@
 
             <ul class="space-y-4">
                 @foreach ($options as $option)
-                    <li>
+                    <li x-data="{
+                        open: true
+                    }">
                         <button
-                            class="px-4 py-2 bg-gray-200 w-full text-left text-gray-700 flex justify-between items-center">
+                            class="px-4 py-2 bg-gray-200 w-full text-left text-gray-700 flex justify-between items-center" x-on:click="open = !open">
                             {{ $option->name }}
 
-                            <i class="fa-solid fa-angle-down"></i>
+                            <i class="fa-solid fa-angle-down" 
+                                x-bind:class="{
+                                    'fa-angle-down': open,
+                                    'fa-angle-up': !open,
+                                }"></i>
 
                         </button>
 
-                        <ul class="mt-2 space-y-2">
+                        <ul class="mt-2 space-y-2" x-show="open">
                             @foreach ($option->features as $feature)
                                 <li>
                                     <label class="inline-flex items-center">

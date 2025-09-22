ea1deebc3864e2b08d1ad698c9d85baf103a9292
Javier Borjas <cristman11@gmail.com>
2025-09-21 22:37:05 -0300

    Escoger la cantidad de items que se comprará

---
+++ app/Livewire/Products/AddToCart.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire\Products;
+
+use Livewire\Component;
+
+class AddToCart extends Component
+{
+    public $product;
+
+    public $qty = 1;
+
+    public function render()
+    {
+        return view('livewire.products.add-to-cart');
+    }
+}
